e4afa4850e72652ea2cfd581dc57696fec36ce18
zehreken <zehreken@gmail.com>
2020-07-04 15:00:12 +0200

    Fix minor issue with quotes array

--- src/main.rs
+++ src/main.rs
@@ -85,8 +85,8 @@ fn get_random_quote() -> String {
     "Sometimes even to live is an act of courage.",
     "All cruelty springs from weakness.",
     "Wherever there is a human being, there is an opportunity for a kindness.",
-    "One of the most beautiful qualities of true friendship is to understand and to be understood."];
-    "Practice does not make perfect. Only perfect practice makes perfect."
+    "One of the most beautiful qualities of true friendship is to understand and to be understood.",
+    "Practice does not make perfect. Only perfect practice makes perfect."];
 
     String::from(quotes[rand::thread_rng().gen_range(0, quotes.len())])
 }
